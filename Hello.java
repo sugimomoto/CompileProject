c714a1d8b76af204b533b2fa060167a14f72b95c
sugimomoto <sugimomoto@gmail.com>
2022-06-20 07:41:42 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello {
+    public static void main(String[] args){
+        int result = plus(1, 5);
+
+        System.out.println(result);
+        
+    }
+
+    public static int plus(int x, int y){
+        return x + y;
+    }
+}
